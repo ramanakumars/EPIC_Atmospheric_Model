e3d45a85759961df51b88d41a32f0a3f878296e6
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2025-06-03 11:28:21 -0400

    adding optional progress indicator

--- src/core/epic_main.c
+++ src/core/epic_main.c
@@ -133,6 +133,7 @@ int main(int   argc,
    *p,*h,
     sum;
   unsigned int
+    progress_frequency = INT_MAX,
     time_index         = 0;
   static double
     *Buff2D[NUM_WORKING_BUFFERS];
@@ -222,6 +223,9 @@ int main(int   argc,
       }
       else if (strcmp(sflag,"-itrun") == 0) {
         sscanf(argv[++count],"%d",&grid.itrun);
+      }
+      else if (strcmp(sflag,"-progress") == 0) {
+        sscanf(argv[++count],"%d",&progress_frequency);
       }
       else {
         if (count == argc-1) {
@@ -438,7 +442,7 @@ int main(int   argc,
       grid.itrun = grid.itime;
     }
 
-    if ( (grid.itime)%(5) == 0 && grid.itime > 0) {
+    if ( (grid.itime)%(progress_frequency) == 0 && grid.itime > 0) {
       if (IAMNODE == NODE0) { 
         fprintf(stdout,"EPIC: Completed timestep %06d\n",grid.itime);
       }
