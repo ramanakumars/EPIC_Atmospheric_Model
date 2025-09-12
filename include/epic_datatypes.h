81d6cc7258ed86eb363b10c07314f8bdba845a88
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2025-09-12 12:48:01 -0700

    fixing N2 output issue

--- include/epic_datatypes.h
+++ include/epic_datatypes.h
@@ -725,7 +725,8 @@ typedef struct {
     phi_surface,          /* geopotential at bottom of model                                */
     gravity2,             /* gravity as a function of layer and latitude [m/s^2]            */
     u_spinup,             /* Rayleigh-drag zonal wind                                 [m/s] */
-    pbot;                 /* pressure at bottom of gas giant, used as a boundary condition  */
+    pbot,                 /* pressure at bottom of gas giant, used as a boundary condition  */
+    Nsquared2;            /* N^2 (Brunt-Vaisalla frequency-squared */
   double_triplet
     *t_cool_table;        /* Profile for time constant used in Newtonian cooling            */
   time_variable
